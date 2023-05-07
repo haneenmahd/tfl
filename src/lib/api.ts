7b4b1844f670429c2ac7709ab76e170299f5e0ec
Haneen Mahdin <haneenmahdin@gmail.com>
2023-05-08 02:36:54 +0530

    chore: remove `APP_KEY`

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,31 +1,18 @@
 import axios, { AxiosResponse } from 'axios';
-import { API_URL, APP_KEY } from './constants';
+import { API_URL } from './constants';
 import { IArrival, IStopPoint } from '../types';
 
 /**
  * Search for stop points based on the parameter passed in and search for it.
  */
 export const getStopPoints = async () => {
-  const response: AxiosResponse<IStopPoint[]> = await axios.get(
-    `${API_URL}/StopPoint/Type/TransportInterchange`,
-    {
-      params: {
-        app_key: APP_KEY
-      }
-    }
-  );
+  const response: AxiosResponse<IStopPoint[]> = await axios.get(`${API_URL}/StopPoint/Type/TransportInterchange`);
 
   return response.data;
 };
 
 export const getArrivals = async (stopPointId: string): Promise<IArrival[]> => {
-  const response: AxiosResponse<IArrival[]> = await axios.get(
-    `${API_URL}/StopPoint/${stopPointId}/Arrivals`
-    , {
-      params: {
-        app_key: APP_KEY
-      }
-    });
+  const response: AxiosResponse<IArrival[]> = await axios.get(`${API_URL}/StopPoint/${stopPointId}/Arrivals`);
 
   return response.data;
 };
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,1 @@
-export const APP_KEY = 'ab44f225dd6e4b8eb60ced9a005de5aa';
 export const API_URL = 'https://api.tfl.gov.uk';
